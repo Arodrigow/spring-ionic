04dadf68d89aad86893348ebcb338b680f1b92ed
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-07-23 10:43:40 -0300

    First commit

---
+++ src/test/java/com/spring/springionic/SpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

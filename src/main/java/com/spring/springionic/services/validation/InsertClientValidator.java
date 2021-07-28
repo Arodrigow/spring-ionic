a1db0c91c19f24f101a15b4e67636b03fc44a4e1
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-07-28 11:31:42 -0300

    [Added] Validation to email already exists

--- src/main/java/com/spring/springionic/services/validation/InsertClientValidator.java
+++ src/main/java/com/spring/springionic/services/validation/InsertClientValidator.java
@@ -5,19 +5,26 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.spring.springionic.domain.Client;
 import com.spring.springionic.domain.enums.ClientType;
 import com.spring.springionic.dto.ClientNewDTO;
+import com.spring.springionic.repositories.ClientRepository;
 import com.spring.springionic.resources.exceptions.FieldMessage;
 import com.spring.springionic.services.validation.utils.BR;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class InsertClientValidator implements ConstraintValidator<InsertClient, ClientNewDTO> {
 
-@Override
-public void initialize(InsertClient ann) {
-}
+    @Autowired
+    private ClientRepository repo;
 
-@Override
-public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
+    @Override
+    public void initialize(InsertClient ann) {
+    }
+
+    @Override
+    public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
 
     List<FieldMessage> list = new ArrayList<>();
     
@@ -28,6 +35,10 @@ public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) 
         list.add(new FieldMessage("cpfOrCnpj", "Invalid CNPJ."));
     }
     
+    Client aux = repo.findByEmail(objDto.getEmail());
+    if(aux != null){
+        list.add(new FieldMessage("email", "Email already in use."));
+    }
 
     for (FieldMessage e : list) {
         context.disableDefaultConstraintViolation();

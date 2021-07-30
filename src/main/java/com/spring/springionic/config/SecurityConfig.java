1f3667e2896d9c8db18e3ac0915c360755b8a178
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-07-30 13:32:36 -0300

    [Done] Authentication - JWT

--- src/main/java/com/spring/springionic/config/SecurityConfig.java
+++ src/main/java/com/spring/springionic/config/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.spring.springionic.config;
 
 import java.util.Arrays;
 
+import com.spring.springionic.secutiry.JWTAuthenticationFilter;
+import com.spring.springionic.secutiry.JwtUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -21,7 +26,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
     private Environment env;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     private static final String[] PUBLIC_MATCHERS = {
         "/h2-console/**",
@@ -43,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
             .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
             .antMatchers(PUBLIC_MATCHERS).permitAll()
             .anyRequest().authenticated();
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         
     }
@@ -52,6 +64,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean

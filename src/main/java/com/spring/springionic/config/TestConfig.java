13b3e1f9bed2390b392719948e5a49f16807b358
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-07-30 11:09:17 -0300

    [Fix] Missing bean on test profile, formatting

--- src/main/java/com/spring/springionic/config/TestConfig.java
+++ src/main/java/com/spring/springionic/config/TestConfig.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 @Profile("test")
@@ -27,5 +29,10 @@ public class TestConfig {
     @Bean
     public EmailService emailService(){
         return new MockEmailService();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() { 
+          return new JavaMailSenderImpl();
     }
 }
